22a47a1eefcd8200d7292544b44e4b0617458531
riverfiend <63566628+riverfiend@users.noreply.github.com>
2025-08-08 21:32:57 +0000

    Modularize test script to help compare behavior against the base game

--- scripts/test_test.rb
+++ scripts/test_test.rb
@@ -3,11 +3,17 @@
 require_relative 'scripts_helper'
 
 DB.extension :pg_array, :pg_advisory_lock, :pg_json, :pg_enum
+def test_bb(player_count = 3)
+  test_exec('1846_barons_of_the_backwaters', player_count)
+end
 
-def test_test(simple = false, overwrite = false, minors = false)
-  game_title = '1846_barons_of_the_backwaters'
+def test_base(player_count = 3)
+  test_exec('1846', player_count)
+end
+
+def test_exec(game_title, player_count)
   game_class = Engine.game_by_title(game_title)
-  players = Array.new(3) { |n| "Player #{n + 1}" }
+  players = Array.new(player_count) { |n| "Player #{n + 1}" }
   game = game_class.new(players)
   
   #corporations = game.send(:init_corporations, game.stock_market)
