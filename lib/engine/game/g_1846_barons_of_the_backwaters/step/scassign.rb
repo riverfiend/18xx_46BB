e34285c6fcc4c2f3857e44ed5190bdbcbe34915c
riverfiend <63566628+riverfiend@users.noreply.github.com>
2025-08-11 22:09:18 +0000

    Create (draft) logic for SWSC and SC assignment

---
+++ lib/engine/game/g_1846_barons_of_the_backwaters/step/scassign.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative '../../../step/assign'
+
+module Engine
+  module Game
+    module G1846BaronsOfTheBackwaters
+      module Step
+        class SCAssign < Engine::Game::G1846::Step::Assign
+        end
+      end
+    end
+  end
+end
---
+++ lib/engine/game/g_1846_barons_of_the_backwaters/step/swscassign.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require_relative '../../../step/assign'
+
+module Engine
+  module Game
+    module G1846BaronsOfTheBackwaters
+      module Step
+        class SWSCAssign < Engine::Game::G1846BaronsOfTheBackwaters::Step::SCAssign
+          def setup
+            @steamboat = @game.swsteamboat
+          end
+
+          def description
+            return super unless blocking_for_steamboat?
+
+            'Assign Southwest Steamboat Company'
+          end
+
+          def help
+            return super unless blocking_for_steamboat?
+
+            assignments = [steamboat_assigned_hex, steamboat_assigned_corp].compact.map(&:name)
+
+            targets = []
+            targets << 'hex' if steamboat_assignable_to_hex?
+            targets << 'corporation or minor' if steamboat_assignable_to_corp?
+
+            help_text = ["#{@steamboat.owner.name} may assign Southwestern Steamboat Company to a new #{targets.join(' and/or ')}."]
+            help_text << " Currently assigned to #{assignments.join(' and ')}." if assignments.any?
+
+            help_text
+          end
+
+          def process_pass(action)
+            raise GameError, "Not #{action.entity.name}'s turn: #{action.to_h}" unless action.entity == @steamboat
+
+            if (ability = @game.abilities(@steamboat, :assign_hexes, time: 'or_start', strict_time: false))
+              ability.use!
+              @log <<
+                if (hex = steamboat_assigned_hex)
+                  "Southwestern Steamboat Company is assigned to #{hex.name}"
+                else
+                  'Southwestern Steamboat Company is not assigned to any hex'
+                end
+            end
+
+            if (ability = @game.abilities(@steamboat, :assign_corporation, time: 'or_start', strict_time: false))
+              ability.use!
+              @log <<
+                if (corp = steamboat_assigned_corp)
+                  "Southwestern Steamboat Company is assigned to #{corp.name}"
+                else
+                  'Southwestern Steamboat Company is not assigned to any corporation or minor'
+                end
+            end
+
+            pass!
+          end
+        end
+      end
+    end
+  end
+end
