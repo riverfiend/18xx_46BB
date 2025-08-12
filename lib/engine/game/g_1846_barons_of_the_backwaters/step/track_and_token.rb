83d0324844156d2de4b4b5fa28a92cf487fb6205
riverfiend <63566628+riverfiend@users.noreply.github.com>
2025-08-12 17:02:55 +0000

    Let Tiles Upgrade

--- lib/engine/game/g_1846_barons_of_the_backwaters/step/track_and_token.rb
+++ lib/engine/game/g_1846_barons_of_the_backwaters/step/track_and_token.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require_relative '../../g_1846/step/track_and_token'
 
 module Engine
   module Game
@@ -6,12 +7,15 @@ module Engine
       module Step
         class TrackAndToken < Engine::Game::G1846::Step::TrackAndToken
           def tokener_available_hex(entity, hex)
-            if ([entity.id, hex.id] == ['IC', 'L8']) &&
+            if ( [entity.id, hex.id] == ['IC', 'L8'] ) &&
                entity.all_abilities.find { |a| a.type == :token }
               return true
             end
-
             super
+          end
+          def process_lay_tile(action) #this is identical to what we should be inheriting elsewhere, but adding it fixed things?
+            lay_tile_action(action)
+            pass! if !can_lay_tile?(action.entity) && @tokened
           end
         end
       end
